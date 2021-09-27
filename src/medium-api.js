6eea3e708e7c8f002b68954718c2e8148fb34388
YashLT224 <yashv@lambdatest.com>
2021-09-28 00:24:00 +0530

    message

---
+++ src/medium-api.js
@@ -0,0 +1,9 @@
+import "./set-public-path";
+// Anything exported from this file is importable by other in-browser modules.
+export function publicApiFunction() {
+  console.log("utility module");
+}
+
+export function abc() {
+  console.log("uthkarsh");
+}
